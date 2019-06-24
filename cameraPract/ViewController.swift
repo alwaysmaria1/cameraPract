5ef95e633edc1844e28adf889272d7b9f6daf587
alwaysmaria1 <alwaysmaria1@gmail.com>
2019-06-24 16:38:49 -0700

    first save

--- cameraPract/ViewController.swift
+++ cameraPract/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let newScreen = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newScreen.delegate = self
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func takePhoto(_ sender: UIButton) {
+        newScreen.sourceType = .camera
+        present(newScreen,animated: true, completion: nil)
+        
+    }
+    
+    @IBOutlet weak var photoTaken: UIImageView!
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            photoTaken.image = selectedImage
+        }
+        newScreen.dismiss(animated: true, completion: nil)
+    }
 
+    @IBAction func library(_ sender: UIButton) {
+        newScreen.sourceType = .photoLibrary
+        present(newScreen,animated: true, completion: nil)
+    }
 }
 
